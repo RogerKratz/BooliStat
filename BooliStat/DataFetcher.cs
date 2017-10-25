0dcca81bbe3f5f38e438777fe296707064f67482
Roger Kratz <roger.kratz@gmail.com>
2017-10-26 00:27:27 +0200

    Adding first few classes

---
+++ BooliStat/DataFetcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using BooliNET;
+
+namespace BooliStat
+{
+    public class DataFetcher
+    {
+        public IEnumerable<Apartment> Execute(string callerId, string privateKey)
+        {
+            var booli = new Booli(callerId, privateKey);
+            var sc = new BooliSearchCondition();
+            sc.Q = "stockholms+innerstad";
+            var result = booli.GetResult(sc);
+            
+            Console.WriteLine(result);
+            return null;
+        }
+    }
+    
+    public class Apartment{}
+}
---
+++ BooliStatTest/Foo.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using BooliStat;
+
+namespace BooliStatTest
+{
+    public class Foo
+    {
+        [Test]
+        public void Testet()
+        {
+            var fetchdata = new DataFetcher();
+            fetchdata.Execute("callerId", "privatekey");
+            
+        }
+        
+    }
+}
